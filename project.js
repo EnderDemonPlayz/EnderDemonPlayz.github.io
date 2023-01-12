ebe8430131a519c0166573f1d52956f41ffb0869
Matthew Lee <107580012+EnderDemonPlayz@users.noreply.github.com>
2023-01-12 13:34:14 -0800

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+let bicycle_or_subway = prompt("Today is the day you head to the grocery store to make some breakfast. Sadly your car is currently under repair so you can either (1) take your bicycle to the store or (2) take the subway");
+if(bicycle_or_subway == 1) {
+    let bicycle_or_subway=prompt("You decided to take your bicycle to the store. After what feels like forever your tire goes out. Oh no now theis nothing we can do. You look around and you can't find anything that could replace your tire. Well guess our only option is to head home");
+    
+} else {
+    let subway_yay = prompt("You decide to go to the subway. Once you make it to the subway you realize you forgot your wallet oh no. Looks like we have only two options (1) go home and get it or (2) go on the subway anyway");
+
+}
+if(subway_yay == 1) {
+    let subway_yay=prompt("You decide to quickly go home in hopes you find your wallet. After what felt like an hour of looking you found your wallet in your pocket. Guess it was with you all this time. Oh well its not breakfast time anymore. Guess we can try another day");
+
+} else {
+    let subway_yay=prompt("You go on the subway and what felt like forever you made it to the store. After buying all the things you need with your wallet you head home happy, that you can cook breakfast.");
+}
